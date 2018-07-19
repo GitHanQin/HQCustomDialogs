81134cb1f1193be668d6c7307b791bbdaf504e82
韩琴 <244629377@qq.com>
2018-07-19 14:50:30 +0800

    111

---
+++ HQCustomDialogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HQCustomDialogs
+//
+//  Created by 韩琴 on 2018/7/19.
+//  Copyright © 2018年 韩琴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
